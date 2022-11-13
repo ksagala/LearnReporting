e5df892936b4cf69f8aaa80ff4aaac49e3429ffb
Konrad Sagala <ksagala@outlook.com>
2022-11-13 22:07:20 +0100

    Create AADUserTable.sql

---
+++ AADUserTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE UserAADInfo (
+    userPrincipalName nvarchar(max),
+    displayName nvarchar(max),
+    surname nvarchar(50),
+    givenName nvarchar(50),
+    mail nvarchar(max),
+    id nvarchar(max),
+    department nvarchar(max),
+    companyName nvarchar(50)
+);
